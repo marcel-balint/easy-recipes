02ed2e0deba362614212207ea62d295078d84fa5
Marcel Balint <blank_blankness@yahoo.com>
2019-06-12 13:22:56 +0000

    Connected my app to database and created 'base.html' file

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 
 
 app = Flask(__name__)
+app.config['MONGO_DBNAME'] = 'recipesProject'
+app.config['MONGO_URI'] = 'mongodb+srv://marcel:Euskaltel@myfirstcluster-r0di4.mongodb.net/recipesProject?retryWrites=true&w=majority'
+
+
+mongo = PyMongo(app)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/home')
+def home():
+    _countries = mongo.db.countries.find()
+    countries_list = [countries for countries in _countries]
+    return render_template('home.html', countries=countries_list)
+    
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
