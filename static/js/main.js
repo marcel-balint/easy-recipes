2820581efbdfa815e909c20323bed2a36aef325f
Marcel Balint <blank_blankness@yahoo.com>
2019-06-23 14:34:40 +0000

    Created functionality to add/remove input fields and styled two buttons

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,25 @@
-
 $(document).ready(function() {
     $('.collapsible').collapsible();
     $('select').material_select();
     $('.button-collapse').sideNav();
 });
+
+	$(document).ready(function() {
+    var wrapper = $("#ingredients"); 
+    var add_button = $("#add"); 
+   
+    var x = 1;
+    
+   $(add_button).click(function(){ // on click add new textarea field
+            $(wrapper).append('<div class="input-field col s12 m12 l12" id="ingredients">' +
+                    '<textarea id="icon_prefix2" class="materialize-textarea" id="ingredients" name="ingredients" required></textarea><a href="#" class="remove-field">Remove</a></div>');
+            x++; 
+    });
+   
+    $(wrapper).on("click",".remove-field", function(e){ //on click remove textarea field
+       e.preventDefault(); 
+		$(this).parent('div').remove(); 
+		x--;
+    });
+});
+
